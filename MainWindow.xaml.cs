d58a5148bfd0a36365ed3df4b62d518bbc36bc00
sohail-raza <sohail.raza.mail@gmail.com>
2019-08-20 22:39:10 +0100

    Clock implemented
    Clock is implemented. Currently only has one format.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Personal_Organiser_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+
+    public partial class MainWindow : Window
+    {
+        string dateFormat = "dddd, dd MMMM yyyy";
+
+
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            InitialiseClock();
+
+
+        }
+
+
+        //Clock init
+        public void InitialiseClock()
+        {
+            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+
+            dispatchLbl.Content = DateTime.Now.ToString(dateFormat);
+
+        }
+
+
+
+    }
+}
---
+++ dispatchTimer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Personal_Organiser_Project
+{
+    class dispatchTimer : MainWindow
+    {
+
+        public void InitialiseClock()
+        {
+            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+           // string dateFormat = "dddd, dd MMMM yyyy";
+
+            dispatchLbl.Content = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+
+        }
+    }
+}
